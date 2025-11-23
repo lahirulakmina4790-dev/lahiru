cd88785c4b7eb7e5db696879fb3891ccb13976e0
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-11-23 05:04:37 -0800

    Update song.js

--- plugins/song.js
+++ plugins/song.js
@@ -11,7 +11,7 @@ cmd(
     filename: __filename,
   },
   async (
-    danuwa,
+    zanta,
     mek,
     m,
     {
@@ -55,7 +55,7 @@ Song downloader
 🔗 *Watch Here:* ${data.url}
 `;
 
-      await danuwa.sendMessage(
+      await zanta.sendMessage(
         from,
         { image: { url: data.thumbnail }, caption: desc },
         { quoted: mek }
@@ -74,7 +74,7 @@ Song downloader
         return reply("⏳ *Sorry, audio files longer than 30 minutes are not supported.*");
       }
 
-      await danuwa.sendMessage(
+      await zanta.sendMessage(
         from,
         {
           audio: { url: songData.download.url },
@@ -83,7 +83,7 @@ Song downloader
         { quoted: mek }
       );
 
-      await danuwa.sendMessage(
+      await zanta.sendMessage(
         from,
         {
           document: { url: songData.download.url },
@@ -94,7 +94,7 @@ Song downloader
         { quoted: mek }
       );
 
-      return reply("Downloaded ✅");
+      return reply("> *වැඩේ හරි 🙃✅*");
     } catch (e) {
       console.log(e);
       reply(`❌ *Error:* ${e.message} 😞`);
