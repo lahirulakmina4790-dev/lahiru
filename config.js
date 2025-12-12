146c6694ff141a668d8722e4b3f309611478b82a
Akashkavindu <143059042+Akashkavindu@users.noreply.github.com>
2025-12-12 08:55:51 -0800

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const FINAL_ALIVE_MSG = ALIVE_MSG_TEMPLATE
 
 
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "xF1nSDQY#tkOzmGabPFFJ0GwR8uOy7mu2479BrINgNGkFxBBC8I8", //Your session id
+SESSION_ID: process.env.SESSION_ID || "JZ91BZKL#SwbHaz1nipWlWu5CicFRT-WtdKPgAZrFogEbAOVBpuE", //Your session id
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Akashkavindu/ZANTA_MD/refs/heads/main/images/ChatGPT%20Image%20Nov%2021%2C%202025%2C%2001_21_32%20AM.png",
 ALIVE_MSG: process.env.ALIVE_MSG || FINAL_ALIVE_MSG, 
 BOT_OWNER: OWNER_NUMBER, 
